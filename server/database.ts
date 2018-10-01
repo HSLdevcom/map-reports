548d2240dd51f8db0f6add9ff8c3c921384db9bf
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-10-01 08:49:55 +0300

    Remove knex stuff, add connection management by env and connection string.

--- server/database.ts
+++ server/database.ts
@@ -1,15 +1,24 @@
 import { createConnection, EntitySchema } from 'typeorm'
 import { ensureAdmin } from './util/ensureAdmin'
 import { User } from './entity/User'
+import { parse } from './util/parsePostgresConnectionString'
 
 const database = async () => {
-  const connection = await createConnection({
-    type: 'postgres',
+  const connectionString = process.env.PG_CONNECTION_STRING
+
+  const defaultConnection = {
     host: 'localhost',
     port: 5432,
     username: 'postgres',
     password: 'mysecretpassword',
     database: 'postgres',
+  }
+
+  const pgConnection = connectionString ? parse(connectionString) : defaultConnection
+
+  const connection = await createConnection({
+    ...pgConnection,
+    type: 'postgres',
     synchronize: true,
     logging: false,
     entities: [__dirname + '/entity/*.ts'],
